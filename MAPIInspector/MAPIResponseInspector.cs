dd090f10079e3e132f28c99ebf0db21bdbf4c6e1
HuiliDong <huilidong2007@163.com>
2015-09-01 13:46:29 +0800

    Upload Jingyu's version

---
+++ MAPIInspector/MAPIResponseInspector.cs
@@ -0,0 +1,20 @@
+﻿using Fiddler;
+
+namespace MapiInspector
+{
+    public class MAPIResponseInspector : MAPIInspector, IResponseInspector2
+    {        
+        public HTTPResponseHeaders headers
+        {
+            get
+            {
+                return this.BaseHeaders as HTTPResponseHeaders;
+            }
+
+            set
+            {
+                this.BaseHeaders = value;
+            }
+        }
+    }
+}
