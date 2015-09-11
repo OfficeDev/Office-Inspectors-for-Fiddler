9db883dced5e0d9ed8aa38294b516f82da40e23e
wangjingfang <wangjingfang825@sina.com>
2015-09-11 17:55:03 +0800

    updated according to comments

--- MAPIInspector/MAPI.js
+++ MAPIInspector/MAPI.js
@@ -1,33 +1,26 @@
-﻿public static BindUIColumn("MAPI")
+﻿public static BindUIColumn("MS Protocol")
 function CalcMethodCol(oS : Session){
     var isMAPI: boolean = false;
-    if (oS.RequestHeaders.ExistsAndContains("Content-Type", "application/mapi-http"))
+    if (oS.RequestHeaders.ExistsAndContains("Content-Type", "application/mapi-http")) {
+        isMAPI = true; 
+    }    else if (oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "application/mapi-http"))
     {
         isMAPI = true; 
     }
-    else if (oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "application/mapi-http"))
-    {
-        isMAPI = true; 
-    }
-    else if (oS.ResponseHeaders.Exists("X-ResponseCode") && !oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "text/html"))
-    {
+    else if (oS.ResponseHeaders.Exists("X-ResponseCode") && !oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "text/html")) {
         isMAPI = true;
     }
-    else
-    {
+    else {
         isMAPI = false; 
     }
 
-    if(isMAPI && oS.RequestHeaders.ExistsAndContains("X-RequestType", "Execute"))
-    {
+    if(isMAPI && oS.RequestHeaders.ExistsAndContains("X-RequestType", "Execute")) {
         return "MS-OXCROPS";  
     }
-    else if (isMAPI)
-    {
+    else if (isMAPI) {
         return "MS-OXCMAPIHTTP";
     }
-    else 
-    {
+    else {
         return "";
     }
 }
