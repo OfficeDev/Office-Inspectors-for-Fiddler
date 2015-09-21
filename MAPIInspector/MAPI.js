deb234c1744984a0c50cd87e2e69e93a9fc43e94
wangjingfang <wangjingfang825@sina.com>
2015-09-21 11:24:05 +0800

    Add protocol name "MS-OXNSPI"

--- MAPIInspector/MAPI.js
+++ MAPIInspector/MAPI.js
@@ -8,14 +8,21 @@ function CalcMethodCol(oS : Session) {
     } else if (oS.ResponseHeaders.Exists("X-ResponseCode") && !oS.ResponseHeaders.ExistsAndEquals("X-ResponseCode", "0") && oS.ResponseHeaders.ExistsAndContains("Content-Type", "text/html")) {
         isMAPI = true;
     } else {
-        isMAPI = false; 
-    }
-
-    if(isMAPI && oS.RequestHeaders.ExistsAndContains("X-RequestType", "Execute")) {
-        return "MS-OXCROPS";  
-    } else if (isMAPI) {
-        return "MS-OXCMAPIHTTP";
-    } else {
         return "";
+    }
+    var sRequestType = oS.RequestHeaders["X-RequestType"];
+    switch (sRequestType) {
+        case "Connect":
+            return "MS-OXCMAPIHTTP";
+        case "Execute":
+            return "MS-OXCROPS";  
+        case "Disconnect":
+            return "MS-OXCMAPIHTTP";
+        case "NotificationWait":
+            return "MS-OXCMAPIHTTP";
+        case "Bind":
+            return "MS-OXNSPI";
+        default:
+            return "";
     }
 }
