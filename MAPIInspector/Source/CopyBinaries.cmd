058e2a6b67c0562ad6de9dcd8ab8591b248d7d02
Stephen Griffin <sgriffin@microsoft.com>
2023-12-05 15:16:01 -0500

    fix binary copy path

--- MAPIInspector/Source/CopyBinaries.cmd
+++ MAPIInspector/Source/CopyBinaries.cmd
@@ -1,6 +1,6 @@
 set PathToBinaries=%1
 set FiddlerPath=%2
 
-xcopy "%PathToBinaries%*.*" %FiddlerPath)%\Inspectors\ /Q /Y
+xcopy "%PathToBinaries%*.*" %FiddlerPath%\Inspectors\ /Q /Y
 xcopy "%PathToBinaries%*.*" %FiddlerPath%\ImportExport\ /Q /Y
 exit
