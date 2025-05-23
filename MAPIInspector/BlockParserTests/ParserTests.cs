3337bc65840ba7c4bc82b017d2e255ccefeeefbf
Stephen Griffin <sgriffin@microsoft.com>
2025-05-23 10:57:45 -0400

    Stub in BlockParser project with unit testing

---
+++ MAPIInspector/BlockParserTests/ParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlockParser.Tests
+{
+    [TestClass()]
+    public class ParserTests
+    {
+        [TestMethod()]
+        public void ParseMAPITest()
+        {
+            // Arrange
+            string expected = "Parsing MAPI data...";
+            
+            // Act
+            string result = Parser.ParseMAPI();
+            
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+	}
+}
---
+++ MAPIInspector/MAPIParser/Parser.cs
@@ -0,0 +1,10 @@
+﻿namespace BlockParser
+{
+    public class Parser
+    {
+        public static string ParseMAPI()
+        {
+            return "Parsing MAPI data...";
+        }
+    }
+}
