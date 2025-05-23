9dd883aa528246d3a33b871d9693543e1fd0915f
Stephen Griffin <sgriffin@microsoft.com>
2025-05-23 12:11:16 -0400

    Move everything where it belongs, stub in BinaryParser and tests

---
+++ MAPIInspector/Source/BlockParser/BinaryParser.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parser
+{
+    // binaryParser - helper class for parsing binary data without
+    // worrying about whether you've run off the end of your buffer.
+    public class BinaryParser
+    {
+        private readonly List<byte> bin;
+        private int offset;
+        private int size; // When uncapped, this is bin.Count. When capped, this is our artificial capped size.
+        private readonly Stack<int> sizes = new Stack<int>();
+
+        public BinaryParser()
+        {
+            bin = new List<byte>();
+            size = 0;
+            offset = 0;
+        }
+
+        public BinaryParser(int cb, byte[] _bin)
+        {
+            if (_bin != null && cb > 0)
+            {
+                if (_bin.Length > cb)
+                {
+                    bin = new List<byte>(new List<byte>(new ArraySegment<byte>(_bin, 0, cb)));
+                }
+                else
+                {
+                    bin = new List<byte>(new List<byte>(_bin));
+                }
+            }
+            else
+            {
+                bin = new List<byte>();
+            }
+
+            size = bin.Count;
+            offset = 0;
+        }
+
+        public BinaryParser(System.IO.Stream stream, int cb = -1)
+        {
+            if (stream == null)
+            {
+                bin = new List<byte>();
+                size = 0;
+                offset = 0;
+                return;
+            }
+
+            long originalPosition = stream.CanSeek ? stream.Position : 0;
+            try
+            {
+                int length = (int)(stream.Length - (stream.CanSeek ? stream.Position : 0));
+                int readLength = (cb > 0 && cb < length) ? cb : length;
+                byte[] buffer = new byte[readLength];
+                int bytesRead = 0;
+                while (bytesRead < readLength)
+                {
+                    int n = stream.Read(buffer, bytesRead, readLength - bytesRead);
+                    if (n == 0) break;
+                    bytesRead += n;
+                }
+                if (buffer.Length > bytesRead)
+                {
+                    bin = new List<byte>(new List<byte>(new ArraySegment<byte>(buffer, 0, bytesRead)));
+                }
+                else
+                {
+                    bin = new List<byte>(buffer);
+                }
+
+                size = bin.Count;
+                offset = 0;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                    stream.Position = originalPosition;
+            }
+        }
+
+        public BinaryParser(List<byte> _bin)
+        {
+            bin = _bin ?? new List<byte>();
+            size = bin.Count;
+            offset = 0;
+        }
+
+        public bool Empty => offset == size;
+        public void Advance(int cb) => offset += cb;
+        public void Rewind() => offset = 0;
+        public int GetOffset() => offset;
+        public void SetOffset(int _offset) => offset = _offset;
+        public byte[] GetAddress()
+        {
+            if (offset >= 0 && offset < bin.Count)
+            {
+                return bin.GetRange(offset, bin.Count - offset).ToArray();
+            }
+            return Array.Empty<byte>();
+        }
+
+        public void SetCap(int cap)
+        {
+            sizes.Push(size);
+            if (cap != 0 && offset + cap < size)
+            {
+                size = offset + cap;
+            }
+        }
+
+        public void ClearCap()
+        {
+            if (sizes.Count == 0)
+            {
+                size = bin.Count;
+            }
+            else
+            {
+                size = sizes.Pop();
+            }
+        }
+
+        // If we're before the end of the buffer, return the count of remaining bytes
+        // If we're at or past the end of the buffer, return 0
+        // If we're before the beginning of the buffer, return 0
+        public int GetSize()
+        {
+            return offset > size ? 0 : size - offset;
+        }
+
+        public bool CheckSize(int cb)
+        {
+            return cb <= GetSize();
+        }
+    }
+}
---
+++ MAPIInspector/Source/BlockParserTests/BinaryParserTests.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+
+namespace Parser.Tests
+{
+	[TestClass]
+	public class BinaryParserTests
+	{
+		[TestMethod]
+		public void Constructor_Empty_Defaults()
+		{
+			var parser = new BinaryParser();
+			Assert.AreEqual(0, parser.GetSize());
+			Assert.IsTrue(parser.Empty);
+		}
+
+		[TestMethod]
+		public void Constructor_ByteArray_CorrectSize()
+		{
+			byte[] data = { 1, 2, 3, 4 };
+			var parser = new BinaryParser(4, data);
+			Assert.AreEqual(4, parser.GetSize());
+			Assert.IsFalse(parser.Empty);
+		}
+
+		[TestMethod]
+		public void Constructor_ByteArray_WithCap()
+		{
+			byte[] data = { 1, 2, 3, 4, 5 };
+			var parser = new BinaryParser(3, data);
+			Assert.AreEqual(3, parser.GetSize());
+		}
+
+		[TestMethod]
+		public void Constructor_Stream_ReadsCorrectly()
+		{
+			byte[] data = { 10, 20, 30, 40 };
+			using (var ms = new MemoryStream(data))
+			{
+				var parser = new BinaryParser(ms);
+				Assert.AreEqual(4, parser.GetSize());
+			}
+		}
+
+		[TestMethod]
+		public void Constructor_Stream_WithCap()
+		{
+			byte[] data = { 10, 20, 30, 40 };
+			using (var ms = new MemoryStream(data))
+			{
+				var parser = new BinaryParser(ms, 2);
+				Assert.AreEqual(2, parser.GetSize());
+			}
+		}
+
+		[TestMethod]
+		public void Constructor_ListByte()
+		{
+			var list = new List<byte> { 1, 2, 3 };
+			var parser = new BinaryParser(list);
+			Assert.AreEqual(3, parser.GetSize());
+		}
+
+		[TestMethod]
+		public void Advance_And_Rewind()
+		{
+			byte[] data = { 1, 2, 3, 4 };
+			var parser = new BinaryParser(4, data);
+			parser.Advance(2);
+			Assert.AreEqual(2, parser.GetOffset());
+			parser.Rewind();
+			Assert.AreEqual(0, parser.GetOffset());
+		}
+
+		[TestMethod]
+		public void SetOffset_Works()
+		{
+			byte[] data = { 1, 2, 3, 4 };
+			var parser = new BinaryParser(4, data);
+			parser.SetOffset(3);
+			Assert.AreEqual(3, parser.GetOffset());
+		}
+
+		[TestMethod]
+		public void GetAddress_ReturnsCorrectBytes()
+		{
+			byte[] data = { 1, 2, 3, 4 };
+			var parser = new BinaryParser(4, data);
+			parser.Advance(2);
+			var addr = parser.GetAddress();
+			CollectionAssert.AreEqual(new byte[] { 3, 4 }, addr);
+		}
+
+		[TestMethod]
+		public void SetCap_And_ClearCap()
+		{
+			byte[] data = { 1, 2, 3, 4, 5 };
+			var parser = new BinaryParser(5, data);
+			parser.Advance(1);
+			parser.SetCap(2);
+			Assert.AreEqual(2, parser.GetSize());
+			parser.ClearCap();
+			Assert.AreEqual(4, parser.GetSize());
+		}
+
+		[TestMethod]
+		public void GetSize_And_CheckSize()
+		{
+			byte[] data = { 1, 2, 3 };
+			var parser = new BinaryParser(3, data);
+			Assert.IsTrue(parser.CheckSize(2));
+			parser.Advance(2);
+			Assert.IsFalse(parser.CheckSize(2));
+		}
+
+		[TestMethod]
+		public void Empty_Property()
+		{
+			byte[] data = { 1, 2 };
+			var parser = new BinaryParser(2, data);
+			Assert.IsFalse(parser.Empty);
+			parser.Advance(2);
+			Assert.IsTrue(parser.Empty);
+		}
+	}
+}
