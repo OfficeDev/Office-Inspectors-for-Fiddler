317683565b3613099c14036c13f440dd4684f2d6
Stephen Griffin <sgriffin@microsoft.com>
2025-05-23 17:28:30 -0400

    Add BlockT

---
+++ MAPIInspector/Source/BlockParser/BlockT.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Parser
+{
+    public class BlockT<T> : Block where T : struct
+    {
+        private T data;
+
+        public BlockT() { }
+
+        public BlockT(T data, int size, int offset)
+        {
+            this.parsed = true;
+            this.data = data;
+            this.SetSize(size);
+            this.SetOffset(offset);
+        }
+
+        public void SetData(T data) => this.data = data;
+        public T GetData() => data;
+        public ref T GetDataRef() => ref data;
+
+        public T Data
+        {
+            get => data;
+            set => data = value;
+        }
+
+        public static BlockT<T> Parse(BinaryParser parser)
+        {
+            var ret = new BlockT<T>();
+            ret.parser = parser;
+            ret.EnsureParsed();
+            return ret;
+        }
+
+        // Construct directly from a parser
+        public BlockT(BinaryParser parser) => Parse<T>(parser);
+
+        // Build and return object of type T, reading from type U
+        public static BlockT<T> Parse<U>(BinaryParser parser) where U : struct
+        {
+            if (!parser.CheckSize(System.Runtime.InteropServices.Marshal.SizeOf(typeof(U))))
+                return new BlockT<T>();
+
+            U uData = ReadStruct<U>(parser);
+            int offset = parser.GetOffset();
+            return Create((T)Convert.ChangeType(uData, typeof(T)), System.Runtime.InteropServices.Marshal.SizeOf(typeof(U)), offset);
+        }
+
+        public static BlockT<T> Create(T data, int size, int offset)
+        {
+            var ret = new BlockT<T>(data, size, offset);
+            ret.parsed = true;
+            return ret;
+        }
+
+        protected override void Parse()
+        {
+            this.parsed = false;
+            int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+            if (!parser.CheckSize(size)) return;
+
+            data = ReadStruct<T>(parser);
+            parser.Advance(size);
+            this.parsed = true;
+        }
+
+        private static U ReadStruct<U>(BinaryParser parser) where U : struct
+        {
+            int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(U));
+            byte[] bytes = parser.ReadBytes(size);
+            var handle = System.Runtime.InteropServices.GCHandle.Alloc(bytes, System.Runtime.InteropServices.GCHandleType.Pinned);
+            try
+            {
+                IntPtr ptr = handle.AddrOfPinnedObject();
+                return (U)System.Runtime.InteropServices.Marshal.PtrToStructure(ptr, typeof(U));
+            }
+            finally
+            {
+                handle.Free();
+            }
+        }
+        public static BlockT<U> EmptyT<U>() where U : struct => new BlockT<U>();
+    }
+}
---
+++ MAPIInspector/Source/BlockParserTests/BlockTTests.cs
@@ -0,0 +1,105 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+
+namespace BlockParserTests
+{
+    [TestClass]
+    public class BlockTTests
+    {
+        private struct TestStruct
+        {
+            public int A;
+            public short B;
+        }
+
+        [TestMethod]
+        public void BlockT_SetAndGetData_Works()
+        {
+            var block = new BlockT<int>();
+            block.SetData(42);
+            Assert.AreEqual(42, block.GetData());
+            block.Data = 99;
+            Assert.AreEqual(99, block.Data);
+        }
+
+        [TestMethod]
+        public void BlockT_GetDataRef_UpdatesValue()
+        {
+            var block = new BlockT<int>();
+            ref int dataRef = ref block.GetDataRef();
+            dataRef = 123;
+            Assert.AreEqual(123, block.GetData());
+        }
+
+        [TestMethod]
+        public void BlockT_Create_SetsProperties()
+        {
+            var block = BlockT<int>.Create(7, 4, 10);
+            Assert.AreEqual(7, block.Data);
+            Assert.AreEqual(4, block.Size);
+            Assert.AreEqual(10, block.Offset);
+        }
+
+        [TestMethod]
+        public void BlockT_EmptyT_ReturnsEmptyBlock()
+        {
+            var empty = BlockT<int>.EmptyT<int>();
+            Assert.IsNotNull(empty);
+            Assert.AreEqual(default(int), empty.Data);
+        }
+
+        [TestMethod]
+        public void BlockT_Parse_ReadsData()
+        {
+            // Arrange
+            var bytes = BitConverter.GetBytes(0x12345678);
+            var parser = new BinaryParser(bytes);
+            var block = BlockT<int>.Parse(parser);
+            Assert.AreEqual(0x12345678, block.Data);
+        }
+
+        // TODO: Implement this test with a more realistic scenario when we need it
+        //[TestMethod]
+        //public void BlockT_ParseU_ReadsAndConverts()
+        //{
+        //    var testStruct = new TestStruct { A = 0x11223344, B = 0x5566 };
+        //    int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(TestStruct));
+        //    byte[] bytes = new byte[size];
+        //    IntPtr ptr = System.Runtime.InteropServices.Marshal.AllocHGlobal(size);
+        //    try
+        //    {
+        //        System.Runtime.InteropServices.Marshal.StructureToPtr(testStruct, ptr, false);
+        //        System.Runtime.InteropServices.Marshal.Copy(ptr, bytes, 0, size);
+        //    }
+        //    finally
+        //    {
+        //        System.Runtime.InteropServices.Marshal.FreeHGlobal(ptr);
+        //    }
+        //    var parser = new BinaryParser(bytes);
+        //    var block = BlockT<long>.Parse<TestStruct>(parser);
+        //    Assert.AreEqual(Convert.ToInt64(testStruct.A), block.Data);
+        //}
+
+        [TestMethod]
+        public void BlockT_ParseT_ReadsAndConverts()
+        {
+            var testStruct = new TestStruct { A = 0x11223344, B = 0x5566 };
+            int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(TestStruct));
+            byte[] bytes = new byte[size];
+            IntPtr ptr = System.Runtime.InteropServices.Marshal.AllocHGlobal(size);
+            try
+            {
+                System.Runtime.InteropServices.Marshal.StructureToPtr(testStruct, ptr, false);
+                System.Runtime.InteropServices.Marshal.Copy(ptr, bytes, 0, size);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.FreeHGlobal(ptr);
+            }
+            var parser = new BinaryParser(bytes);
+            var block = BlockT<TestStruct>.Parse(parser);
+            Assert.AreEqual(testStruct, block.Data);
+        }
+    }
+}
