857666eca95516cfd2b717aae0a431a7f797a9e0
Stephen Griffin <sgriffin@microsoft.com>
2025-06-21 22:26:56 -0400

    Convert more

--- MAPIInspector/Source/Parsers/MSOXCDATA/2.11 Property Values/TypedString.cs
+++ MAPIInspector/Source/Parsers/MSOXCDATA/2.11 Property Values/TypedString.cs
@@ -1,32 +1,29 @@
 ﻿namespace MAPIInspector.Parsers
 {
-    using System.IO;
-    using System.Text;
+    using BlockParser;
 
     /// <summary>
     /// 2.11.7 TypedString Structure
     /// </summary>
-    public class TypedString : BaseStructure
+    public class TypedString : Block
     {
         /// <summary>
         /// An enum value of StringType
         /// </summary>
-        public StringTypeEnum StringType;
+        public BlockT<StringTypeEnum> StringType;
 
         /// <summary>
         /// If the StringType field is set to 0x02, 0x03, or 0x04, then this field MUST be present and in the format specified by the Type field. Otherwise, this field MUST NOT be present.
         /// </summary>
-        public MAPIString String;
+        public Block String;
 
         /// <summary>
         /// Parse the TypedString structure.
         /// </summary>
-        /// <param name="s">A stream containing the TypedString structure</param>
-        public override void Parse(Stream s)
+        protected override void Parse()
         {
-            base.Parse(s);
-            StringType = (StringTypeEnum)ReadByte();
-            switch (StringType)
+            StringType = ParseT<StringTypeEnum>();
+            switch (StringType.Data)
             {
                 case StringTypeEnum.NoPresent:
                 case StringTypeEnum.Empty:
@@ -34,31 +31,27 @@
                         String = null;
                         break;
                     }
-
                 case StringTypeEnum.CharacterString:
-                    {
-                        String = new MAPIString(Encoding.ASCII);
-                        String.Parse(s);
-                        break;
-                    }
-
                 case StringTypeEnum.ReducedUnicodeCharacterString:
                     {
-                        String = new MAPIString(Encoding.ASCII);
-                        String.Parse(s);
+                        String = ParseStringA();
                         break;
                     }
-
                 case StringTypeEnum.UnicodeCharacterString:
                     {
-                        String = new MAPIString(Encoding.Unicode);
-                        String.Parse(s);
+                        String = ParseStringW();
                         break;
                     }
-
                 default:
                     break;
             }
+        }
+
+        protected override void ParseBlocks()
+        {
+            SetText("TypedString");
+            AddChildBlockT(StringType, "StringType");
+            if (String != null) AddChild(String, $"String:{String}");
         }
     }
 }
